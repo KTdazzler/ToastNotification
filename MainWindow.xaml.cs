ce37d9510d487c85d986bc7aef2c8867f08bf273
TsungChe <congjhe@gmail.com>
2018-11-12 15:09:49 +0800

    Create Toast Notification project
    Add C# Sample code for Toast Notification in Windows

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using System.Management.Automation;
+using System.Management.Automation.Runspaces;
+
+namespace MyToastNotification
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MyToastNotifications _toast = null;
+        private String _appID = String.Empty;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Toast_Click(object sender, RoutedEventArgs e)
+        {
+            if (_toast == null )
+            {
+                // Get App ID
+                using (PowerShell powershell = PowerShell.Create())
+                {
+                    powershell.AddScript("Get-StartApps");
+
+                    foreach (PSObject result in powershell.Invoke())
+                    {
+                        // Find MyToastNotification.exe APP_ID
+                        if (result.Members["Name"].Value.ToString().Contains("MyToastNotification"))
+                        {
+                            _appID = result.Members["AppID"].Value.ToString();
+                        }
+                    }
+                }
+
+            }
+
+            if (!String.IsNullOrEmpty(_appID))
+            {
+                // Send Toast Notification
+                _toast = new MyToastNotifications(_appID);
+                _toast.ShowNotification(String.IsNullOrEmpty(_tbCaption.Text) ? "MyCaption" : _tbCaption.Text,
+                        String.IsNullOrEmpty(_tbMessage.Text) ? "MyMessage" : _tbMessage.Text);
+            }
+            else
+            {
+                MessageBox.Show("Can't find App_ID. Please create a shortcut and copy to \"C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\" ");
+            }
+
+        }
+    }
+}
---
+++ MyToastNotification.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+using Windows.Data.Xml.Dom;
+using Windows.UI.Notifications;
+
+namespace MyToastNotification
+{
+    internal class MyToastNotifications
+    {
+        private string _appID = string.Empty;
+        public string AppID { get { return _appID; } }
+
+        public MyToastNotifications(string appid) : base()
+        {
+            _appID = appid;
+        }
+
+        public bool ShowNotification(string caption = "", string message = "")
+        {
+            bool ret = false;
+
+            // Create toast XML
+            XmlDocument toastXml = new XmlDocument();
+            toastXml.LoadXml(string.Format("<toast launch='app-defined-string'>" +
+                                                "<visual>" +
+                                                    "<binding template='ToastGeneric'>" +
+                                                        "<image placement='appLogoOverride' hint-crop='circle' src='{0}' />" +
+                                                        "<text >{1}</text>" +
+                                                        "<text >{2}</text>" +
+                                                        "<text placement='attribution'> Via MyToast </text>" +
+                                                    "</binding>" +
+                                                "</visual>" +
+                                                "<actions>" +
+                                                    "<action content='Yes' arguments='MyYes'/>" +
+                                                    "<action content='No' arguments='MyNo' />" +
+                                                "</actions>" +
+                                            "</toast>",
+                                string.Format(@"{0}\{1}", Environment.CurrentDirectory, "AppLogo.jpg"),
+                                caption,
+                                message));
+
+            ToastNotification newToast = new ToastNotification(toastXml);
+
+            // Add event
+            newToast.Activated += OnActivated;
+            newToast.Dismissed += OnDismissed;
+
+            try
+            {
+                if (!string.IsNullOrEmpty(_appID))
+                {
+                    ToastNotificationManager.CreateToastNotifier(_appID).Show(newToast);
+
+                    ret = true;
+                }
+            }
+            catch (Exception e)
+            {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        private void OnDismissed(ToastNotification sender, ToastDismissedEventArgs args)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(delegate ()
+            {
+                 System.Windows.MessageBox.Show("OnDismissed:: Reason = " + args.Reason);
+            });
+        }
+
+        private void OnActivated(ToastNotification sender, object args)
+        {
+            System.Windows.Application.Current.Dispatcher.Invoke(delegate ()
+            {
+                if (args != null && (args is ToastActivatedEventArgs))
+                {
+                    var arg = args as ToastActivatedEventArgs;
+                    switch (arg.Arguments)
+                    {
+                        case "MyYes":
+                            System.Windows.MessageBox.Show("OnActivated:: You click \"Yes\"");
+                            break;
+                        case "MyNo":
+                            System.Windows.MessageBox.Show("OnActivated:: You click \"No\"");
+                            break;
+                    }
+                }
+            });
+        }
+    }
+}
